0643d57f4ecd52b1a16e5152acabc941ce886cdf
Javi Martín <javim@elretirao.net>
2025-02-05 01:46:54 +0100

    TEMP2

--- app/models/custom/setting.rb
+++ app/models/custom/setting.rb
@@ -1,12 +1,14 @@
+load Rails.root.join("app", "models", "abilities", "common.rb")
+
 module Abilities
   class Common
-    include CanCan::Ability
+    alias_method :consul_initialize, :initialize # create a copy of the original method
 
     def initialize(user)
-      # Copy 100 lines of code here
-      # can :create, Comment # Comment or delete the original line
+      consul_initialize(user) # call the original method
+      cannot :create, Comment # undo the permission added in the original method
 
-      if user.level_two_or_three_verified? # Add your code
+      if user.level_two_or_three_verified?
         can :create, Comment
       end
     end
